207af6a4ec123c7dcf05268601112f32efcaad09
felit <felit123456>
2016-10-27 19:42:35 +0800

    修改布署脚本

--- deploy/config/deploy.rb
+++ deploy/config/deploy.rb
@@ -1,14 +1,14 @@
 # config valid only for current version of Capistrano
 lock '3.5.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'yunpan_crawler'
+set :repo_url, 'git@github.com:felit/pan_crawler.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
+set :deploy_to, '/var/crawler'
 
 # Default value for :scm is :git
 # set :scm, :git
